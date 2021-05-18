72abfda31c1d762c9564c26ca248c32fb7e1d5f0
Vera <verakut9@gmail.com>
2021-05-18 21:00:15 +0300

    localisation

--- src/main/java/ru/kpfu/itis/kutyavina/styleweb/config/WebConfig.java
+++ src/main/java/ru/kpfu/itis/kutyavina/styleweb/config/WebConfig.java
@@ -1,14 +1,19 @@
 package ru.kpfu.itis.kutyavina.styleweb.config;
 
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import ru.kpfu.itis.kutyavina.styleweb.logging.LoggingAspect;
 import ru.kpfu.itis.kutyavina.styleweb.models.Capsule;
 import ru.kpfu.itis.kutyavina.styleweb.utils.StringConverter;
 
+import java.util.Locale;
 import java.util.logging.Logger;
 
 @Configuration
@@ -27,6 +32,24 @@ public class WebConfig {
     @Bean
     public StringConverter idolGenericConverter(){
         return new StringConverter(Capsule.class);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource res = new ReloadableResourceBundleMessageSource();
+        res.setBasenames("classpath:i18n/messages", "classpath:i18n/validation");
+        res.setCacheSeconds(0);
+        res.setDefaultEncoding("UTF-8");
+        res.setUseCodeAsDefaultMessage(false);
+        return res;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setCookieName("lang");
+        localeResolver.setDefaultLocale(new Locale("en", "US"));
+        return localeResolver;
     }
 
 }
--- src/main/java/ru/kpfu/itis/kutyavina/styleweb/controllers/MainController.java
+++ src/main/java/ru/kpfu/itis/kutyavina/styleweb/controllers/MainController.java
@@ -1,14 +1,27 @@
 package ru.kpfu.itis.kutyavina.styleweb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
 import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private LocaleResolver localeResolver;
 
     @PermitAll
     @GetMapping("/mainpage")
@@ -25,5 +38,13 @@ public class MainController {
     public String root() {
 
         return "redirect:mainpage";
+    }
+
+    @RequestMapping("/changehome/{locale}")
+    public String changeHome(@PathVariable("locale") String locale, HttpServletRequest request, HttpServletResponse response){
+        String[] localeData = locale.split("_");
+        localeResolver.setLocale(request, response, new Locale(localeData[0], localeData[1]));
+        String referer = request.getHeader("Referer");
+        return "redirect:"+ referer;
     }
 }
